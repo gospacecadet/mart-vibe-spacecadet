6f75b956f241febc4d8f2de5af46f3d9ca9d6248
Marvin Arnold <marvinmarnold@gmail.com>
2016-02-01 02:25:26 -0600

    more email

--- packages/mart-vibe-spacecadet/forbiddens/dockings/manage-dockings-forbid.js
+++ packages/mart-vibe-spacecadet/forbiddens/dockings/manage-dockings-forbid.js
@@ -2,3 +2,11 @@ Template.manageDockingsForbid.onCreated(function() {
   if(!Mart.canActAsMerchant())
     forbid()
 })
+
+Template.reserveDockingDetailsCart.onCreated(function() {
+  if(Meteor.userId() !== Template.currentData().userId)
+    forbid()
+
+  if(Template.currentData().state !== Mart.Cart.STATES.SHOPPING)
+    forbid()
+})
